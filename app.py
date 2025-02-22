00361be6a1d40432f1683e7fbfda420eed7d59a5
Kulbhushan <kulbhushan.mayer@gmail.com>
2025-02-22 16:47:54 +0530

    Update Application to 8000

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def home():
     return render_template_string(html_content)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8000)
